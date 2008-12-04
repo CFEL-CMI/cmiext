8c7170020b6b516fa30bf6f49a17e13161bb6383
Jochen Küpper <jochen.kuepper@cfel.de>
2008-12-04 11:26:51 +0000

    start this copy of constants for systems without pyGSL
    git-svn-id: https://svnsrv.desy.de/desy/jkext/trunk@11 6ca46338-6782-4cce-8b50-0e9d5d11f351

---
+++ lib/const.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 Jochen Küpper <software@jochen-kuepper.de>
+
+"""Provide physical constants.
+
+If pyGSL is avialable, we will simply import its constants into our namespace.
+
+Otherwise, we will provide "copies" of the most important constants,
+using the same naming convention and the same values.
+
+Values last updated: $Date:$"""
+
+try:
+    from pygsl.const import *
+except:
+    angstrom = 1e-10
+    pi = 3.1415926535897931
+    plancks_constant_h = 6.6260689599999996e-34
+    unified_atomic_mass = 1.6605387820000001e-27
+    
+
+### Local Variables:
+### fill-column: 132
+### End:
