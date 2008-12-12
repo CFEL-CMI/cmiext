84930f9d800b8c83f8f28ef89942174030dfc421
Jochen Küpper <jochen.kuepper@cfel.de>
2008-12-12 21:07:37 +0000

    add example script for plotting Stark curves
    git-svn-id: https://svnsrv.desy.de/desy/jkext/trunk@55 6ca46338-6782-4cce-8b50-0e9d5d11f351

---
+++ scripts/starkeffect_plot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# This file is part of JK Python extensions
+# Copyright (C) 2008 Jochen Küpper <software@jochen-kuepper.de>
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# If you use this programm for scientific work, you must correctly reference it; see LICENSE file for details.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# some SGE commands for batch usage convenience
+#$ -cwd
+#$ -e $JOB_NAME.sge
+#$ -o $JOB_NAME.sge
+#$ -S $HOME/.python/bin/python
+#$ -V
+from __future__ import division
+
+"""Demonstration script to plot Stark curves from HDF file
+
+Copyright (C) 2008 Jochen Küpper"""
+
+__author__ = "Jochen Küpper <software@jochen-kuepper.de>"
+
+import numpy as num
+import sys
+import matplotlib.pyplot as plt
+
+import jkext.molecule as molecule
+from jkext.convert import *
+from jkext.state import State
+
+# create Molecule object and specify storage file
+storagename = "molecule.hdf"
+mol = molecule.Molecule(storage=storagename)
+
+plt.clf()
+for J in range(0, 4):
+    Ka = 0
+    for Kc in range(J, -1, -1):
+        state = State(J, Ka, Kc, 0, 0)
+        fields, energies = mol.starkeffect(state)
+        plt.plot(V_m2kV_cm(fields), J2Hz(energies) / 1e6)
+        if Kc > 0:
+            Ka += 1
+            state = State(J, Ka, Kc, 0, 0)
+            fields, energies = mol.starkeffect(state)
+            plt.plot(V_m2kV_cm(fields), J2Hz(energies) / 1e6)
+
+plt.show()
