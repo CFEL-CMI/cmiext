54c20d893d66d56ffe0ad1b2b18094482f4ce815
Jochen Küpper <jochen.kuepper@cfel.de>
2009-04-09 14:18:03 +0000

    install public scripts
    git-svn-id: https://svnsrv.desy.de/desy/jkext/trunk@76 6ca46338-6782-4cce-8b50-0e9d5d11f351

--- setup.py
+++ setup.py
@@ -29,5 +29,7 @@ setup(name="jkext",
       long_description    = long_description,
       package_dir         = {'jkext': 'lib'},
       packages            = ['jkext'],
+      scripts             = ['scripts/jkext_calculate_energy', 'scripts/jkext_plot_energy',
+                             'scripts/jkext_print_energy']
       )
 
