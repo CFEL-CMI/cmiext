5964a40063ccfe2806c0f5c138d97f14c0850b20
Jochen Küpper <jochen.kuepper@cfel.de>
2009-09-29 18:49:06 +0000

    some (formatting) cleanup
    git-svn-id: https://svnsrv.desy.de/desy/jkext/branches/mixed_field_branch@152 6ca46338-6782-4cce-8b50-0e9d5d11f351

--- lib/convert.py
+++ lib/convert.py
@@ -65,30 +65,37 @@ def J2invcm(val):
 
 
 def V_m2kV_cm(val):
-    """V/m -> kV/cm"""
+    """V / m -> kV / cm"""
     return numpy.array(val) * 1e-5
 
 
 def kV_cm2V_m(val):
-    """kV/cm -> V/m"""
+    """kV / cm -> V / m"""
     return numpy.array(val) / 1e-5
 
+
 def A32CM2_V(val):
-    """Å -> CM^2/V"""
+    """Å^3 -> C M^2 / V"""
     return val*const.vacuum_permittivity*((const.angstrom)**3)*4*const.pi
 
-def dcfields2omega(dcfields,rotcon,dipole):
-    
+
+def dcfields2omega(dcfields, rotcon, dipole):
+    """ToDo: document"""
     return numpy.array(dcfields)*dipole/rotcon
 
-def acfields2deltaomega(acfields,rotcon,polarizability):
 
+def acfields2deltaomega(acfields, rotcon, polarizability):
+    """ToDo: document"""
     return acfields**2*polarizability/(4*rotcon)
 
-def omega2dcfields(omega,rotcon,dipole):
-    assert dipole != 0
+
+def omega2dcfields(omega, rotcon, dipole):
+    """ToDo: document"""
+    assert 0 != dipole
     return numpy.array(omega)*rotcon/dipole
 
-def deltaomega2acfields(deltaomega,rotcon,polarizability):
-    assert polarizability != 0
+
+def deltaomega2acfields(deltaomega, rotcon, polarizability):
+    """ToDo: document"""
+    assert 0 != polarizability
     return (numpy.array(deltaomega)*4*rotcon/polarizability)**0.5
