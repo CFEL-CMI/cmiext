63d33bfaa1d8883bff66a6b71c0e159526f3b63b
Jochen Küpper <jochen.kuepper@cfel.de>
2009-10-06 15:47:24 +0000

    updated wigner_gsl wrapper - works with Python 2.6 (but not 2.5 from MacPorts) - provide error estiates through drot_e
    git-svn-id: https://svnsrv.desy.de/desy/jkext/branches/mixed_field_branch@180 6ca46338-6782-4cce-8b50-0e9d5d11f351

--- src/wigner_gsl_module.c
+++ src/wigner_gsl_module.c
@@ -14,23 +14,45 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, U
 */
 
 #include <Python.h>
+#include <gsl/gsl_errno.h>
+
 #include "wigner_gsl.h"
 
 static PyObject *_wigner_drot(PyObject *self, PyObject *args)
 {
     int j, m1, m2, status;
-    double theta, result;
+    double theta;
+    gsl_sf_result result;
     if (!PyArg_ParseTuple(args, "iiid", &j, &m1, &m2, &theta))
         return NULL;
-    if(status = gsl_sf_wigner_drot_e(j, m1, m2, theta, &results)) {
+    gsl_set_error_handler_off();
+    if(status = gsl_sf_wigner_drot_e(j, m1, m2, theta, &result)) {
         /* error */
+        PyErr_SetString(PyExc_RuntimeError, "GSL error in gsl_sf_wigner_drot_e");
     }
-    return Py_BuildValue("d", result);
+    return Py_BuildValue("d", result.val);
+}
+
+
+static PyObject *_wigner_drot_e(PyObject *self, PyObject *args)
+{
+    int j, m1, m2, status;
+    double theta;
+    gsl_sf_result result;
+    if (!PyArg_ParseTuple(args, "iiid", &j, &m1, &m2, &theta))
+        return NULL;
+    gsl_set_error_handler_off();
+    if(status = gsl_sf_wigner_drot_e(j, m1, m2, theta, &result)) {
+        /* error */
+        PyErr_SetString(PyExc_RuntimeError, "GSL error in gsl_sf_wigner_drot_e");
+    }
+    return Py_BuildValue("dd", result.val, result.err);
 }
 
 
 static PyMethodDef _wigner_methods[] = {
     {"drot", _wigner_drot, METH_VARARGS, "Reduced Wigner d rotation matrix."},
+    {"drot_e", _wigner_drot_e, METH_VARARGS, "Reduced Wigner d rotation matrix with error bounds."},
     {NULL, NULL, 0, NULL}
 };
 
@@ -38,5 +60,4 @@ static PyMethodDef _wigner_methods[] = {
 PyMODINIT_FUNC init_wigner_gsl(void)
 {
     (void) Py_InitModule("_wigner_gsl", _wigner_methods);
-    gsl_set_error_handler_off();
 }
