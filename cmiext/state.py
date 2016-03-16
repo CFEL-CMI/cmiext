63168c969cb91ba2a6b81c74b858f54639903570
Jochen Küpper <jochen.kuepper@cfel.de>
2016-03-16 12:25:48 +0200

    partly implement symptop-sign handling -- should work for all but the HDF name functions

--- cmiext/state.py
+++ cmiext/state.py
@@ -37,25 +37,26 @@ class State:
     def __init__(self, J=0, Ka=0, Kc=0, M=0, isomer=0):
         self.max = 1000
         self.__initialize(J, Ka, Kc, M, isomer)
+        self.__symtop_sign = 1
 
     def __initialize(self, J=0, Ka=0, Kc=0, M=0, isomer=0):
         """Store all info and reata a unique ID for the state.
 
-        For symmetric tops, K is stored in the natural choice of Ka or Kc. The state depends on the sign of the product
-        of K*M, which is stored in the decimal places 15 and 16 (0-14 being used to encode J, Ka, Kc, M, isomer).
+        For symmetric tops, :math:`K` is stored in the natural choice of :math:`K_a` or :math:`K_c`. The state depends
+        on the sign of the product of :math:`K\cdot{}M`, which is stored in the decimal place 15 (0-14 being used to
+        encode J, Ka, Kc, M, isomer).
+
         """
         assert ((0 <= J < self.max) and (abs(Ka) < self.max) and (abs(Kc) < self.max) and (0 <= M < self.max)
                 and (0 <= isomer < self.max))
-        self.__labels = num.array([J, Ka, Kc, M, isomer], dtype=num.int64)
-        self.__id = num.uint64(0)
-        self.__symtop_sign = 1
+        self.__labels = np.array([J, Ka, Kc, M, isomer], dtype=np.int64)
+        self.__id = np.uint64(0)
         for i in range(self.__labels.size):
-            self.__id += num.uint64(abs(self.__labels[i]) * self.max**i)
+            self.__id += np.uint64(abs(self.__labels[i]) * self.max**i)
         # handle negative sign of symmetric-top K*M
-        for i in (1,2):
-            if self.__labels[i] < 0:
-                self.__symtop_sign = -1
-                self.__id += num.uint64(self.max**5 * 10**(i-1))
+        if Ka * Kc * M < 0:
+            self.__symtop_sign = -1
+            self.__id += np.uint64(1e15)
 
     def J(self):
         return self.__labels[0]
@@ -79,30 +80,33 @@ class State:
         if "Kc" == forbidden and self.Kc() % 2 == 1: return 0
         return 1
 
+    def symtop_sign():
+        return self.__symtop_sign
+
     def fromid(self, id):
         """Set quantum-numbers form id"""
-        self.__id = num.int64(id)
-        self.__labels = num.zeros((5,), dtype=num.int64)
+        id = np.uint64(id)
+        self.__id = id
+        self.__labels = np.zeros((5,), dtype=np.int64)
         for i in range(5):
-            self.__labels[i] = self.__id % self.max
-            self.__id //= self.max
+            self.__labels[i] = id % self.max
+            id //= self.max
         # handle negative sign of symmetric-top K*M
-        for i in (1,2):
-            if 1 == self.__id % 10:
-                self.__symtop_sign = -1
-                self.__labels[i] *= -1
-            self.__id //= 10
+        self.__symtop_sign = id % 10
+        id //= 10
         return self
 
     def fromhdfname(self, hdfname):
         """Set quantum-numbers form hdf name.
 
         See hdfname() below for a description of the format.
+
+        .. todo:: Implement symtop-sign usage
         """
         name = hdfname.replace("n","-")
-        qn = num.array(name.split("/"))
+        qn = np.array(name.split("/"))
         J, Ka, Kc, M, iso = qn.tolist()
-        self.__initialize(num.int64(J[1:]), num.int64(Ka[1:]), num.int64(Kc[1:]), num.int64(M[1:]), num.int64(iso[1:]))
+        self.__initialize(np.int64(J[1:]), np.int64(Ka[1:]), np.int64(Kc[1:]), np.int64(M[1:]), np.int64(iso[1:]))
         return self
 
     def id(self):
@@ -116,6 +120,8 @@ class State:
 
         Prepend '_' to all numbers to make them valid Python identifiers. We split the individual quantum numbers by '/'
         in order to provide subgrouping for faster transversal of the HDF5 directory.
+
+        .. todo:: Implement symtop-sign usage
         """
         name = "_%d/_%d/_%d/_%d/_%d" % self.totuple()
         name.replace("-","n")
